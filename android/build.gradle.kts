7dd3a49d6aeb646e45c06ecd7a0d4f57cb8fecb9
jothishankar17 <jothi.shankar@mugilsystems.com>
2025-10-07 13:02:40 +0530

    Updated build.gradle.kts file

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,6 +20,42 @@
 //     delete(rootProject.layout.buildDirectory)
 // }
 
+//-------------------------------------------------
+
+// allprojects {
+//     repositories {
+//         google()
+//         mavenCentral()
+//     }
+// }
+
+// val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+// rootProject.layout.buildDirectory.set(newBuildDir)
+
+// subprojects {
+//     val newSubprojectBuildDir = newBuildDir.dir(project.name)
+//     project.layout.buildDirectory.set(newSubprojectBuildDir)
+// }
+
+// subprojects {
+//     project.evaluationDependsOn(":app")
+// }
+
+// tasks.register<Delete>("clean") {
+//     delete(rootProject.layout.buildDirectory)
+// }
+
+
+//-------------------------------------------------------------------------------------------------
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    id("com.android.application") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    // ✅ Add this line for Firebase
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -42,3 +78,4 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
